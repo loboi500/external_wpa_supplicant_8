af3c67a901b56600d47c7a85f0a9d64c886d91ee
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-02 22:45:21 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,12 +1,3 @@
-LOCAL_PATH:= $(call my-dir)
-HS20_LOCAL_PATH:=$(LOCAL_PATH)
-
-ifneq ($(filter VER_0_8_X VER_2_1_DEVEL,$(WPA_SUPPLICANT_VERSION)),)
-# The order of the 2 Android.mks does matter!
-# TODO: Clean up the Android.mks, reset all the temporary variables at the
-# end of each Android.mk, so that one Android.mk doesn't depend on variables
-# set up in the other Android.mk.
-include $(LOCAL_PATH)/hostapd/Android.mk \
-        $(LOCAL_PATH)/wpa_supplicant/Android.mk
-endif
-include $(HS20_LOCAL_PATH)/hs20/client/Android.mk
+version https://git-lfs.github.com/spec/v1
+oid sha256:ff464b6104278609d4841b027e0b909e9f0ad584d1620d5065ba9c554f9ce8c6
+size 507
